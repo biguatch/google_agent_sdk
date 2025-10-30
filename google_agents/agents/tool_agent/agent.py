eda059b181ea127536883061af214065a8ba1ec1
Bill Yildirim <biguatch@gmail.com>
2025-10-30 11:46:14 +0000

    greeting agent and tool agent

---
+++ google_agents/agents/tool_agent/agent.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+
+from google.adk.agents import Agent
+
+
+def get_current_time() -> dict:
+    """
+    Get the current time in the format YYYY-MM-DD HH:MM:SS
+    """
+
+    return {"current_time": datetime.now().strftime("%Y-%m-%d %H:%M:%S")}
+
+
+root_agent = Agent(
+    model="gemini-2.0-flash",
+    name="tool_agent",
+    description="Tool agent",
+    instruction="""
+    You are a helpful assistant that use the following tools:
+        - get_current_time
+    """,
+    # tools=[google_search],
+    tools=[get_current_time],
+)
